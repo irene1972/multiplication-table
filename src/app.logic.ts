22ea64673361d224b1db448c3a6bea7e848ed7e2
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-23 12:39:33 +0200

    redo app.logic

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -1,25 +1,30 @@
 import fs from 'fs';
 import { yarg } from '../config/plugins/args.plugin';
 
-console.log(yarg);
-const {b:base, l:limit, s:showTable} = yarg;
-let outputMessage='';
-//const base=5;
-const headerMessage=`
----------------------------------
-        Tabla del ${base}
----------------------------------\n
+const { b:base, l:limit, s:showTable } = yarg;
+let outputMessage = '';
+const headerMessage = `
+==================================
+       Tabla del ${ base }
+==================================\n
 `;
 
-outputMessage=headerMessage+outputMessage;
-
-if(showTable){
-    console.log(outputMessage);
+for( let i = 1; i <= limit; i++ ) {
+  outputMessage += `${ base } x ${ i } = ${ base * i }\n`;
 }
 
-const outputPath=`outputs`;
+outputMessage = headerMessage + outputMessage;
 
-fs.mkdirSync(outputPath,{recursive:true});
+if ( showTable ) {
+  console.log(outputMessage);
+}
 
-fs.writeFileSync(`${outputPath}/tabla-${base}.txt`,outputMessage);
-console.log('File created');
+const outputPath = `outputs`;
+
+
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${ outputPath }/tabla-${ base }.txt`, outputMessage);
+console.log('File created!');
+
+// grabar en el archivo de salida
+// path: outputs/tabla-5.txt
